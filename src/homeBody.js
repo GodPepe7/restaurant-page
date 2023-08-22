765915320b2a46f57fe42a558ef321245b61a886
GodPepe7 <privatalexm@gmail.com>
2023-08-22 22:20:18 +0200

    Add homeBody Module

---
+++ src/homeBody.js
@@ -0,0 +1,26 @@
+import './homeBody.css'
+
+const homeBody = () => {
+    const homeBodyDiv = document.createElement('div');
+    homeBodyDiv.className = 'home-body';
+
+    const heading = document.createElement('h2');
+    heading.textContent = 'Original Italian Pizza';
+
+    const flavorDiv = document.createElement('div');
+    flavorDiv.className = 'flavor-div';
+
+    const flavorText = document.createElement('p');
+    flavorText.textContent = `Our restaurant takes pride in crafting 
+    pizzas that are profoundly delicious and truly based on the rich 
+    traditions of Italian culinary excellence.`;
+    const quoteAuthor = document.createElement('p');
+    quoteAuthor.textContent = '- Trust me bro';
+
+    flavorDiv.append(flavorText, quoteAuthor);
+    homeBodyDiv.append(heading, flavorDiv);
+
+    return homeBodyDiv;
+};
+
+export default homeBody;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import contentDiv from "./contentDiv.css"
 import pageLoad from "./page-load";
 import headerDiv from "./header";
+import homeBody from "./homeBody";
 
 pageLoad(headerDiv());
+pageLoad(homeBody());
